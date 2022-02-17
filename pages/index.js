36316da07e80317c2d82da22d308a6102f7f40eb
Anuradha Kumari <2anuradha9@gmail.com>
2022-02-17 23:55:26 +0100

    add twitter card meta

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,13 @@ export default function Home() {
         <title>Learn SEO</title>
         <meta name="description" content="Learn SEO - meta, sitemaps, open graph. Make your websites rank higher in search results!" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@miracle_404" />
         <meta name="og:title" content="Learn Search Engine Optimisation" />
         <meta name="og:description" content="Learn SEO - meta, sitemaps, open graph. Make your websites rank higher in search results!" />
         <meta name="og:type" content="website" />
-        <meta name="og:image" content="https://learn-seo-next.vercel.app/learn-seo.png" />
-        <meta name="og:url" content="https://learn-seo-next.vercel.app/" />
+        <meta name="og:image" content="https://learn-seo-twitter-card.vercel.app/learn-seo.png" />
+        <meta name="og:url" content="https://learn-seo-twitter-card.vercel.app/" />
       </Head>
 
       <main className={styles.main}>
